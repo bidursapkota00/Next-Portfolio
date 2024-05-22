b6065ddef42892255cf708e714b84fdbff4f84db
bidursapkota00 <bidursapkota00@gmail.com>
2024-05-22 12:47:14 +0545

    trying mailto and tel

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -17,7 +17,11 @@ export default function Contact() {
             <div>
               <FaEnvelopeOpenText size={20} color="rgb(44, 152, 240)" />
             </div>
-            <h2>bidursapkota00@gmail.com</h2>
+            <h2>
+              <a href="mailto:bidursapkota00@gmail.com">
+                bidursapkota00@gmail.com
+              </a>
+            </h2>
           </div>
           <div className="contact__information">
             <div>
@@ -29,7 +33,9 @@ export default function Contact() {
             <div>
               <FaPhone size={20} color="rgb(44, 152, 240)" />
             </div>
-            <h2>+977 9865711881</h2>
+            <h2>
+              <a href="tel:+9779865711881">+977 9865711881</a>
+            </h2>
           </div>
         </div>
 
